d5214759bfc47f3cca70892cb13d36cc64ee141e
Joe Paterson <iamtempme@gmail.com>
2020-10-23 23:29:50 +0100

    created install script and minor adjustments to repo cmd file

--- repo.cmd
+++ repo.cmd
@@ -4,25 +4,25 @@ goto main
 
 
 :help
-echo ==================================================
+echo ====================================================
 echo USAGE
 echo =====
 echo repo.cmd [command] [options]
-echo ==================================================
+echo ====================================================
 echo Commands 
 echo --help - This help text
 echo --create [repository-name] - Create a new repository 
 echo --delete [repository-name] - Delete a repository
-echo ==================================================
+echo ====================================================
 echo Repository names cannot contain spaces or special 
 echo characters
-echo ==================================================
+echo ====================================================
 goto :eof
 
 
 :create
 set reponame=%2
-if [%reponame%]==[] echo No repository name provided. Please use 'repo.cmd help' to see usage && pause
+if [%reponame%]==[] echo No repository name provided. Please use 'repo.cmd help' to see usage && pause && exit
 echo # %reponame% > README.md
 python main.py --create %2
 git init
@@ -31,25 +31,25 @@ git commit -m "Repository Initialised"
 git branch -M main
 git remote add origin https://github.com/%username%/%reponame%.git
 git push -u origin main
-pause
+exit
 goto :eof
 
 
 :delete
 set reponame=%2
-if [%reponame%]==[] echo No repository name provided. Please use 'repo.cmd help' to see usage && pause
+if [%reponame%]==[] echo No repository name provided. Please use 'repo.cmd help' to see usage && exit
 python main.py --delete %2
 goto :eof
 
 
 
 :main
-if [%1]==[] echo No argument supplied. Please use 'help' to see usage && pause
+if [%1]==[] echo No argument supplied. Please use 'help' to see usage && exit
 if "%1"=="help" goto :help
 if [%1]==[c] goto :create
 if [%1]==[create] goto :create
 if [%1]==[d] goto :delete
 if [%1]==[delete] goto :delete
-pause
+exit
 
-pause
+exit
